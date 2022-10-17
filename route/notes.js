d4ab8e30e30feaa8df6cf621189c7185748e0b87
BenLiu <liumanpan1990@hotmail.com>
2022-10-16 20:13:25 -0400

    complete POST and Delete API

---
+++ route/notes.js
@@ -0,0 +1,51 @@
+const notes = require("express").Router();
+const uuid = require("uuid");
+const { readAndAppend, readFromFile } = require("../helpers/fsUtils");
+const fs = require("fs");
+
+
+notes.get("/", (req, res) => {
+
+    readFromFile("./db/db.json", 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+        } else {
+
+            res.json(JSON.parse(data));
+        }
+    })
+
+})
+
+notes.post("/", (req, res) => {
+
+    const { title, text } = req.body;
+    const id = uuid.v4();
+    const data = { title, text, id };
+
+    readAndAppend(data, "./db/db.json");
+    setTimeout(() => { res.json(data) }, 100);
+
+})
+
+notes.delete("/:id", (req, res) => {
+    const id = req.params.id;
+
+    readFromFile("./db/db.json", 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+        } else {
+
+            let Data = JSON.parse(data);
+            const newData = Data.filter((obj) => obj.id != id)
+            fs.writeFile("./db/db.json", JSON.stringify(newData, null, 4), (err) =>
+                err ? console.error(err) : console.info(`some data deleted`)
+            );
+        }
+    })
+    setTimeout(() => { res.json("deleted") }, 100);
+
+})
+
+
+module.exports = notes;
---
+++ server.js
@@ -0,0 +1,28 @@
+
+const express = require("express");
+const api = require("./route/index")
+const { clog } = require('./middleware/clog');
+const PORT = 3001 || process.env.PORT;
+
+const path = require('path');
+const app = express();
+
+app.use(clog);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use('/api', api);
+
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"))
+})
+
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/notes.html"))
+})
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} `)
+);
